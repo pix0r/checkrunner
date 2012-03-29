357356304251f194419b5191768a1db7c9ee38a1
Mike Matz <mike@pixor.net>
2012-03-29 09:49:58 -0700

    Restructure and added fancy setup.py stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='checkrunner',
+        version='0.1',
+        url='https://github.com/pix0r/checkrunner',
+        py_modules=['checkrunner'],
+        author='Mike Matz',
+        author_email='mike@pixor.net',
+        )
